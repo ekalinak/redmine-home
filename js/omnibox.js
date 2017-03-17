ekalinak/redmine-home

commit cf3a9a1a1bca1fd98c0b85bcddc1a5c1f9cbb754

#6: Improved UI of omnibox when no projets are parsed. Also added missing resources.

js/omnibox.js
@@ -21,7 +21,7 @@ Redmine.Omnibox = {
     },
     getProjects : function(){
         if ( typeof(localStorage.projects) == 'undefined' ) {
-            localStorage.projects = {};
+            localStorage.projects = JSON.stringify({});
         }
         return JSON.parse(localStorage.projects);
     },
@@ -30,12 +30,20 @@ Redmine.Omnibox = {
         var suggest = [];
         var projects = this.getProjects();
 
-        for ( i in projects ) {
-            if ( projects[i].content.toLowerCase().indexOf(pattern.toLowerCase()) != -1
-                || projects[i].description.toLowerCase().indexOf(pattern.toLowerCase()) != -1 ) {
-                suggest.push(projects[i]);
+        if ( !projects.lenght ) {
+            suggest.push({
+                    content: 'No projects: ', 
+                    description: 'There are no parsed projects yet. Please go to OPTIONS and parse projects locally.'
+            });
+        } else {
+            for ( i in projects ) {
+                if ( projects[i].content.toLowerCase().indexOf(pattern.toLowerCase()) != -1
+                    || projects[i].description.toLowerCase().indexOf(pattern.toLowerCase()) != -1 ) {
+                    suggest.push(projects[i]);
+                }
             }
         }
+        
         return suggest;
     },
     getCustomSuggestion: function( text ){

css/custom.css
@@ -1,0 +1,13 @@
+.top-buffer { margin-top: 20px; }
+.is-loading > div { visibility: hidden; }
+.is-loading > div:after { 
+	background: url(../res/loading.gif) 50% 50% no-repeat;
+    background-size: 20px;
+    content: '';
+    width: 100%;
+    height: 20px;
+    display: block;
+    visibility: visible;
+    text-align: center;
+}
+.is-loading span { display: none; }

commit 8053ccd627853278ddebd738cbe44bf8680ed5fa

Fixed stupid bug introduced in cf3a9a1.

js/omnibox.js
@@ -30,7 +30,7 @@ Redmine.Omnibox = {
         var suggest = [];
         var projects = this.getProjects();
 
-        if ( !projects.lenght ) {
+        if ( Object.keys(projects).length == 0 ) {
             suggest.push({
                     content: 'No projects: ', 
                     description: 'There are no parsed projects yet. Please go to OPTIONS and parse projects locally.'