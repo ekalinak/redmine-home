14f2ca4963b199d05d31192bbbcb57181217c616
Erik Kalinak <erik.kalinak@cs2.ch>
2018-01-10 15:08:01 +0100

    Added bigger task limit. ( previously only 25 tasks )

--- js/newTab.js
+++ js/newTab.js
@@ -149,7 +149,8 @@ define(['jquery','knockoutLib','tooltip','theme-init','bootstrapLib'],function($
                 chrome.storage.sync.get('options',function(items){
                     self.options(items.options);
                     self.refreshingStatus(true);
-                    var ajaxUrl = items.options['redmineUrl'] + 'issues.json?assigned_to_id=me&sort=priority:desc,id:asc';
+                    // TODO: Make "limit" configurable
+                    var ajaxUrl = items.options['redmineUrl'] + 'issues.json?assigned_to_id=me&sort=priority:desc,id:asc&limit=100';
                     $.ajax({
                         contentType : 'application/json',
                         headers : {
